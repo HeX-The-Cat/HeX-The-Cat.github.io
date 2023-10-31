cd588307f45c0c92176dbcc0a8f2d88b96281291
HeX The Cat <60222984+HeX-The-Cat@users.noreply.github.com>
2023-10-31 19:12:35 +0200

    stuff again

--- src/layout/pages/home/Home.js
+++ src/layout/pages/home/Home.js
@@ -4,18 +4,23 @@ const Home = () => {
             <h1 className=" text-center">
                 This webpage is a personal project for learning coding and test bed.
             </h1>
-                <p className=" text-center">
+            <p className=" text-center">
                 <br/>
                 Feel free to browse around, maybe something catches your fancy.<br/>
                 <br/>
+            </p>
+            <p className=" text-center">
+                Made tool for quick searching basic Pokémon info using <u><a href="https://pokeapi.co/" target="_blank" rel="noopener noreferrer">PokeAPI</a></u>.<br/>
+                Mainly tooled for phone usage while playing Pokémon myself, but have gotten very good feedback from friends trying it out as well.<br/>
+                Find it&nbsp;<u><a href="https://hex-the-cat.github.io/pokemon/" target="_blank" rel="noopener noreferrer">here</a></u>.
+                <br/><br/>
             </p>
             <p className=" text-center">
                 Also created website for&nbsp;<u><a href="https://brokencodegames.github.io/" target="_blank" rel="noopener noreferrer">Broken Code Games</a></u> as a part of uni's work trainee period.<br/>
                 They wanted older looking web page so that's what I tried my best to do.<br/>
                 The development of the game has sadly come to an end without release.
                 <br/><br/>
-                Whole page is going through rewrite from React to Svelte, but this old React version will do until I manage to iron out some kinks from the new code.<br/>
-                ThreeJS is causing some odd curves on the way..
+                Plans for rewriting the webpage into more presentable condition.. underway..
             </p>
         </div>
     );
--- src/popup_components/PopUp.js
+++ src/popup_components/PopUp.js
@@ -8,7 +8,7 @@ const PopUp = (props) => {
             <div className="popup-inner border border-white rounded-xl">
                 <div className=" md:flex justify-center">
                     <img className="md:shrink-0 px-1 object-none" src={ket} />
-                    <p className="px-1 flex text-center">This page is being developed by person with sleep schedule of a cat, please have patience. I give no guarantee over how this thing works on mobile, currently deving is focused on desktop. Also, this page has embeds, so there is likely cookies involved.</p>
+                    <p className="px-1 flex text-center">This page is being developed by person with sleep schedule of a cat, please have patience. I give no guarantee over how this thing works on mobile, currently deving is focused on desktop.</p>
                 </div>
                 <div className="flex justify-center paddingUpwards">
                     <button className="close-btn px-1 text-xl button" onClick={() => props.setTrigger(false)}> Close </button>
